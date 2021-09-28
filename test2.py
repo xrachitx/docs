06d0bb1e4b344f0dc9eb049a4e86064ff017f5ca
xrachitx <rachit18302@iiitd.ac.in>
2021-09-28 16:44:46 +0530

    changes

--- test2.py
+++ test2.py
@@ -54,7 +54,7 @@ def main():
     batch_size = 10
     dataset = DOCS_Data("./data/","Aeroplane")
     train_loader1 = DataLoader(dataset=dataset,batch_size=1,shuffle=True)
-    print(train_loader1)
+    print("LOADER: ",train_loader1)
     
     # img_path = "./data/Images/Aeroplane/Partial/"
     # gt_path = "./data/Skeletons/Aeroplane/Partial/"
